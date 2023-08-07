e64c01279c7f62be36487deb7344ce97630350b4
zhp <zhp@qq.com>
2023-08-07 18:59:12 +0800

    add dln proof

--- proof/proof.go
+++ proof/proof.go
@@ -10,10 +10,12 @@ import (
 	"errors"
 	"fmt"
 	"github.com/zhp12543/zk-proof/curve"
+	"github.com/zhp12543/zk-proof/dln"
 	"github.com/zhp12543/zk-proof/paillier"
 	"github.com/zhp12543/zk-proof/prime"
 	"math/big"
 	"runtime"
+	"sync"
 	"time"
 
 )
@@ -131,4 +133,78 @@ consumer:
 		Q:          q,
 	}
 	return preParams, nil
+}
+
+func (pk *PaillierParams) VerifyDln( dln1 [][]byte, dln2 [][]byte) error {
+	errChain := make(chan error, 0)
+	doneChain := make(chan struct{}, 0)
+
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		dlnProof1, err := dln.UnmarshalDLNProof(dln1)
+		if err != nil {
+			errChain <- err
+			return
+		}
+
+		if !dlnProof1.Verify(pk.H1i, pk.H2i, pk.NTildei) {
+			errChain <- errors.New("dln1 verify false")
+			return
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		dlnProof2, err := dln.UnmarshalDLNProof(dln2)
+		if err != nil {
+			errChain <- err
+			return
+		}
+
+		if !dlnProof2.Verify(pk.H2i, pk.H1i, pk.NTildei) {
+			errChain <- errors.New("dln2 verify false")
+			return
+		}
+	}()
+
+	go func() {
+		wg.Wait()
+		close(doneChain)
+	}()
+
+	select {
+	case err := <-errChain:
+		return err
+	case <-doneChain:
+		return nil
+	}
+}
+
+func (pk *PaillierParams) DlnProof() ([][]byte, [][]byte, error) {
+	dln1, err := dln.NewDLNProof(
+		pk.H1i,
+		pk.H2i,
+		pk.Alpha,
+		pk.P,
+		pk.Q,
+		pk.NTildei).Serialize()
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	dln2, err := dln.NewDLNProof(
+		pk.H2i,
+		pk.H1i,
+		pk.Beta,
+		pk.P,
+		pk.Q,
+		pk.NTildei).Serialize()
+
+	if err != nil {
+		return nil, nil, err
+	}
+	return dln1, dln2, nil
 }
